a6ddee923f3eb19b1f4d97bbd32040fb4002f674
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-10-19 14:26:49 -0700

    Adjusted styles for login.

--- bucket-list/src/components/Login.js
+++ bucket-list/src/components/Login.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth.js';
-import styled from 'styled-components';
+import { Form, Input, Button } from './styles/LoginStyles.js';
 
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    width: 30%;
-    border: 1px solid red;
-    align-items: center;
-    margin: 10px auto;
-`
-
-const Input = styled.input`
-    width: 80%;
-    padding: 4px;
-    margin: 10px;
-    border: none;
-    border-bottom: 2px solid #a5c3c6;
-    color: #454245;
-    font-size: 1.2rem;
-    text-align: center;
-`
-
-const Button = styled.button`
-    width: 80%;
-    font-size: 1rem;
-    margin: 10px;
-    padding: 4px;
-    border: none;
-    font-size: 1.2rem;
-    background: #dfcdc4;
-
-    &:hover{
-        background: #a5c3c6;
-    }
-`
-// .btn-grad {background-image: linear-gradient(to right, #a49989 0%, #d2cdc4 51%, #a49989 100%)}
-// .btn-grad:hover { background-position: right center; }
 class Login extends React.Component {
     state = {
         credentials : {
