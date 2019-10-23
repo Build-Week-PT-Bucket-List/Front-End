6d750293586d65ac2971b6867fe4b1f5e406a487
garybot <argot.pryer@gmail.com>
2019-10-22 19:17:37 -0700

    added api call to Dashboard to retrieve User data.

--- bucket-list/src/components/Dashboard.js
+++ bucket-list/src/components/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth.js';
+import BucketListGrid from './BucketListGrid.js';
 
 const Dashboard = () => {
+    const [uid, setUid] = useState();
+    useEffect(() => {
+      axiosWithAuth()
+        .get('user')
+        .then(res => {
+          console.log("User:", res);
+          setUid(res.data.user.id);
+        })
+        .catch(err => console.log(err))
+    }, [])
     return(
         <>
         <h1>Hello, World!</h1>
+        <BucketListGrid uid={uid}/>
         </>
     )
 }
